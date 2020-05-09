2ed7063627298b563fccf79767870f3bcf886681
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-05-09 23:15:19 +0900

    feat : add db api

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from .views import hiAPI
+from .views import gu
+from .views import header
 
 urlpatterns = [
-    path('hi/', hiAPI)
+    path('hi/', hiAPI),
+    path('<int:number>', gu),
 ]
--- weather/views.py
+++ weather/views.py
@@ -7,6 +7,11 @@ from .models import Gu3
 from .models import Gu4
 from .models import Header
 from .serializers import Gu0Serializer
+from .serializers import Gu1Serializer
+from .serializers import Gu2Serializer
+from .serializers import Gu3Serializer
+from .serializers import Gu4Serializer
+from .serializers import HeaderSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -15,7 +20,53 @@ def hiAPI(request):
 
 
 @api_view(['GET'])
-def gu_0(request):
-    totaldata = Gu0.objects.all()
+def gu(request, number):
+    if number == 0: totaldata = Gu0.objects.all()
+    elif number == 1: totaldata = Gu1.objects.all()
+    elif number == 2: totaldata = Gu2.objects.all()
+    elif number == 3: totaldata = Gu3.objects.all()
+    elif number == 4: totaldata = Gu4.objects.all()
     serializer = Gu0Serializer(totaldata, many=True)
     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def gu_0(request):
+#     totaldata = Gu0.objects.all()
+#     serializer = Gu0Serializer(totaldata, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def gu_1(request):
+#     totaldata = Gu1.objects.all()
+#     serializer = Gu0Serializer(totaldata, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def gu_2(request):
+#     totaldata = Gu2.objects.all()
+#     serializer = Gu0Serializer(totaldata, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def gu_3(request):
+#     totaldata = Gu3.objects.all()
+#     serializer = Gu0Serializer(totaldata, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def gu_4(request):
+#     totaldata = Gu4.objects.all()
+#     serializer = Gu0Serializer(totaldata, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def header(request):
+#     totaldata = Header.objects.all()
+#     serializer = HeaderSerializer(totaldata, many=True)
+#     return Response(serializer.data)
