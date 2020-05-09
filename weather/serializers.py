a23ded952947e13d5461dd6bcf47ea88e5ec2d83
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-05-09 23:06:37 +0900

    docs : update srializers.py

--- weather/serializers.py
+++ weather/serializers.py
@@ -10,6 +10,41 @@ from .models import Header
 class Gu0Serializer(serializers.ModelSerializer):
     class Meta:
         model = Gu0
-        fields = ()
+        fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
+        'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+
+class Gu1Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gu1
+        fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
+        'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+
+class Gu2Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gu2
+        fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
+        'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+
+class Gu3Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gu3
+        fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
+        'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+
+class Gu4Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gu4
+        fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
+        'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+
+class HeaderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Header
+        fields = ('num', 'update_year', 'update_month', 'update_date')
 
 
