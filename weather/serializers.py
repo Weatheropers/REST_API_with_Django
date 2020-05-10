809467b891a2c2d205fdcb564d221e2553e313b5
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-05-10 11:58:21 +0900

    fix : edit serializers.py

--- weather/serializers.py
+++ weather/serializers.py
@@ -12,35 +12,35 @@ class Gu0Serializer(serializers.ModelSerializer):
         model = Gu0
         fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
         'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
-        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust', 'small_fine_dust')
 
 class Gu1Serializer(serializers.ModelSerializer):
     class Meta:
         model = Gu1
         fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
         'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
-        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust', 'small_fine_dust')
 
 class Gu2Serializer(serializers.ModelSerializer):
     class Meta:
         model = Gu2
         fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
         'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
-        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust', 'small_fine_dust')
 
 class Gu3Serializer(serializers.ModelSerializer):
     class Meta:
         model = Gu3
         fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
         'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
-        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust', 'small_fine_dust')
 
 class Gu4Serializer(serializers.ModelSerializer):
     class Meta:
         model = Gu4
         fields = ('num', 'gu', 'time_difference', 'hour', 'temp', 'day_high_temp', 'day_low_temp', 'sky_state', 
         'weather_state', 'weather_kor', 'rain_persent', 'expect_rain_6h', 'expect_rain_12h', 'expect_snow_6h', 
-        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust')
+        'expect_snow_12h', 'wind_speed', 'wind_way', 'wind_text', 'humi', 'fine_dust', 'small_fine_dust')
 
 class HeaderSerializer(serializers.ModelSerializer):
     class Meta:
